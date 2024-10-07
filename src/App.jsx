0bd5a44992f6d8375656e6fd53cf2f45d87d4121
AsimMasood99 <macbookpro2089@gmail.com>
2024-10-07 22:44:53 +0500

    created a card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import Navbar from "./components/Navbar";
 import ScrollList from "./components/ScrollList";
 import Searchbar from "./components/Searchbar";
+import Card from "./components/Card";
+import { useEffect } from "react";
+// import "./data.json";
+
+const cardData = [];
+
 function App() {
+	useEffect(() => {
+		fetch("/data.json") // Fetching from the public folder
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error("Network response was not ok");
+				}
+				return res.json();
+			})
+			.then((resJson) => {
+				console.log(resJson); // This should now log your JSON data
+			})
+			.catch((error) => {
+				console.error("There was a problem with the fetch operation:", error);
+			});
+	}, []);
 	return (
 		<>
 			<Navbar />
@@ -13,6 +34,7 @@ function App() {
 					<ScrollList />
 				</div>
 			</div>
+			<div></div>
 		</>
 	);
 }
