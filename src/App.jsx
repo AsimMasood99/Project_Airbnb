1ffc0cb15c3a66074ca4ce2471f766d0058b674d
AsimMasood99 <macbookpro2089@gmail.com>
2024-10-07 23:17:36 +0500

    used the props in card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import Navbar from "./components/Navbar";
 import ScrollList from "./components/ScrollList";
 import Searchbar from "./components/Searchbar";
 import Card from "./components/Card";
-import { useEffect } from "react";
-
-const cardData = [];
+import { useEffect, useState } from "react";
 
 function App() {
+	const [cardData, setCardData] = useState([]);
+
 	useEffect(() => {
 		fetch("/data.json")
 			.then((res) => {
 				return res.json();
 			})
 			.then((resJson) => {
-				resJson.forEach((item) => cardData.push(item));
+				setCardData(resJson);
 			});
 	}, []);
 	return (
@@ -27,21 +27,24 @@ function App() {
 					<ScrollList />
 				</div>
 			</div>
-			<div>
-				{cardData.map((item, index) => {
-					return (
-						<Card
-							key={index}
-							cardInfo={{
-								img: item.data,
-								title: item.title,
-								type: item.type,
-								price: item.price,
-								rating: item.rating,
-							}}
-						/>
-					);
-				})}
+			<div className="flex justify-center mt-28">
+				<div className="grid grid-cols-3 gap-10">
+					{cardData.map((item, index) => {
+						console.log(item);
+						return (
+							<Card
+								key={index}
+								cardInfo={{
+									img: item.img,
+									title: item.title,
+									type: item.type,
+									price: item.price,
+									rating: item.rating,
+								}}
+							/>
+						);
+					})}
+				</div>
 			</div>
 		</>
 	);
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { FaHeart, FaShieldAlt, FaBolt, FaFistRaised } from "react-icons/fa"; // Font Awesome icons
 
-function Card() {
-	return <div className="bg-slate-900 h-96 w-96 rounded-3xl"> 
-    <img src="" alt="" className="h-[70%] w-full bg-black"/>
-    <div>
-      <div>
-        <span>title</span>
-        <span>type</span>
-      </div>
-      <div>
-        100$/night
-      </div>
-      <div>
-        Rating
-      </div>
-    </div>
-  </div>
+function Card({ cardInfo }) {
+  {console.log(cardInfo.img)}
+	return (
+		<div className="h-96 w-96 rounded-3xl">
+			<img src={cardInfo.img} alt="" className="h-[70%] w-full" />
+			<div>
+				<div>
+					<span>{cardInfo.title}</span>
+					<span>{cardInfo.type}</span>
+				</div>
+				<div>{cardInfo.price} per Person</div>
+				<div>{cardInfo.rating}</div>
+			</div>
+		</div>
+	);
 }
 
 export default Card;
