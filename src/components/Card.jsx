351fed201ac4a5d5e0d21c3315f383f93a2c36b8
AsimMasood99 <macbookpro2089@gmail.com>
2024-10-07 23:30:18 +0500

    Styling of Card Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { FaHeart, FaShieldAlt, FaBolt, FaFistRaised } from "react-icons/fa"; // Font Awesome icons
+import { IoStar } from "react-icons/io5";
 
 function Card({ cardInfo }) {
   {console.log(cardInfo.img)}
 	return (
-		<div className="h-96 w-96 rounded-3xl">
-			<img src={cardInfo.img} alt="" className="h-[70%] w-full" />
-			<div>
-				<div>
-					<span>{cardInfo.title}</span>
+		<div className="h-96 w-96 hover:scale-105 duration-[350ms] cursor-pointer">
+			<img src={cardInfo.img} alt="image" className="h-[70%] w-full rounded-xl hover:sca"  />
+			<div className="font-medium px-2 pt-4">
+				<div className="flex justify-between">
+					<span className="text-xl">{cardInfo.title}</span>
 					<span>{cardInfo.type}</span>
 				</div>
-				<div>{cardInfo.price} per Person</div>
-				<div>{cardInfo.rating}</div>
+				<div>{cardInfo.price}$ per Person</div>
+				<div className="flex gap-2 items-center">
+        <div>{cardInfo.rating}</div>
+        <IoStar />
+        </div>
 			</div>
 		</div>
 	);
