83deee390e6d51cd01e855e248e5b5fa047a75f0
AsimMasood99 <macbookpro2089@gmail.com>
2024-10-07 22:58:16 +0500

    Fetching data from json as a side effect use useEffec and passing it as a prop to card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,17 @@ import ScrollList from "./components/ScrollList";
 import Searchbar from "./components/Searchbar";
 import Card from "./components/Card";
 import { useEffect } from "react";
-// import "./data.json";
 
 const cardData = [];
 
 function App() {
 	useEffect(() => {
-		fetch("/data.json") // Fetching from the public folder
+		fetch("/data.json")
 			.then((res) => {
-				if (!res.ok) {
-					throw new Error("Network response was not ok");
-				}
 				return res.json();
 			})
 			.then((resJson) => {
-				console.log(resJson); // This should now log your JSON data
-			})
-			.catch((error) => {
-				console.error("There was a problem with the fetch operation:", error);
+				resJson.forEach((item) => cardData.push(item));
 			});
 	}, []);
 	return (
@@ -34,7 +27,22 @@ function App() {
 					<ScrollList />
 				</div>
 			</div>
-			<div></div>
+			<div>
+				{cardData.map((item, index) => {
+					return (
+						<Card
+							key={index}
+							cardInfo={{
+								img: item.data,
+								title: item.title,
+								type: item.type,
+								price: item.price,
+								rating: item.rating,
+							}}
+						/>
+					);
+				})}
+			</div>
 		</>
 	);
 }
