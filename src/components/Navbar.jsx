2a45f117f6362d855f3b47d7546ef7b64a5b41f0
AsimMasood99 <macbookpro2089@gmail.com>
2024-10-07 21:12:46 +0500

    Created Navbar and searchbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaAirbnb } from "react-icons/fa";
+import { LiaOilCanSolid } from "react-icons/lia";
+import { TbBrandAmongUs } from "react-icons/tb";
+import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
-  return (
-    <nav>
-      <div> 
-        {/* LOGO Comes here */}
-      </div>
+	const [selected, setSelected] = useState("Stays");
 
-    </nav>
-  )
+	const changeSelected = (e) => {
+		console.log("hi");
+		console.log(e.target);
+		if (e.target.innerText == "Stays") setSelected("Stays");
+		else setSelected("Exp");
+	};
+
+	return (
+		<nav className="text-xl font-family-roboto flex justify-between items-center px-5 py-5">
+			<div className="flex items-center gap-3">
+				<FaAirbnb className="text-4xl" />
+				<span className="font-bold">My BNB</span>
+			</div>
+			<div className="flex gap-6 font-medium">
+				<div
+					onClick={(e) => changeSelected(e)}
+					className={selected === "Stays" ? "" : "text-gray-500"}
+				>
+					Stays
+				</div>
+				<div
+					onClick={(e) => changeSelected(e)}
+					className={selected === "Exp" ? "" : "text-gray-500"}
+				>
+					Experiences
+				</div>
+			</div>
+			<div className="flex items-center gap-2 bg-gray-300 py-1 px-3 border border-black rounded-full">
+				<IoMenu className="text-3xl" />
+				<TbBrandAmongUs className="text-4xl" />
+			</div>
+		</nav>
+	);
 }
 
-export default Navbar
-
+export default Navbar;
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+
+function Searchbar() {
+	return (
+		<>
+			<div className="flex justify-center">
+				<div className="flex h-16 justify-center items-center w-1/3 gap-2 border-2 border-gray-500 bg-gray-300 rounded-full">
+					<input className="w-full h-full bg-gray-300 rounded-full px-5 " placeholder="Search Location" type="text" name="" id="" />
+					<FaSearch className="cursor-pointer text-2xl bg-slate-700 rounded-full h-full px-6 w-[15%] my-5" />
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Searchbar;
